e840c0199f5f37a96ec1834662a3b24a8501b5da
your name <92675947+Tandu2003@users.noreply.github.com>
2023-10-03 13:27:03 +0700

    add firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBNWWpceDoDVf2yRLpW7iVU3vp59VDPYmw",
+  authDomain: "netflix-4edd1.firebaseapp.com",
+  projectId: "netflix-4edd1",
+  storageBucket: "netflix-4edd1.appspot.com",
+  messagingSenderId: "997492634558",
+  appId: "1:997492634558:web:6ad6d7d4860ad69e6a3fef",
+};
